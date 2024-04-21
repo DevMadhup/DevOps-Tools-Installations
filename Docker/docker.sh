1dffc0826668ae7b1bca72a8a143bebf99a0f8c3
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-04-21 19:36:11 +0530

    Create docker.sh

---
+++ Docker/docker.sh
@@ -0,0 +1,17 @@
+## Amazon Ubuntu
+#!/bin/bash
+sudo apt update -y
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+
+sudo apt update -y
+
+apt-cache policy docker-ce -y
+
+sudo apt install docker-ce -y
+
+sudo usermod -aG docker $USER && newgrp docker
