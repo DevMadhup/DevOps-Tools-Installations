fa9874818de6658b1fa4083b4e408773f39fb825
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-04-21 19:40:04 +0530

    Create jenkins.sh

---
+++ Jenkins/jenkins.sh
@@ -0,0 +1,14 @@
+sudo apt update -y
+sudo apt upgrade -y 
+sudo apt install fontconfig openjdk-17-jre -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+  
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+
