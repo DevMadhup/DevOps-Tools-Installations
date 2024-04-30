c61c9169ebccda70fef375c3f00601c520cb1a3c
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-04-30 10:59:47 +0530

    Create Kubectl.sh

---
+++ Kubectl/Kubectl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version --short --client
