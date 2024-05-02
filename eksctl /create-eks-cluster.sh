a30f87c427b0fc7960f4c0155d05c2e88a574fe7
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-05-02 17:11:51 +0530

    Update create-eks-cluster.sh

--- eksctl /create-eks-cluster.sh
+++ eksctl /create-eks-cluster.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
-eksctl create cluster --name=wanderlust \       #Change as per your requirement
-                      --region=us-east-1 \      #Change as per your requirement
-                      --without-nodegroup
+eksctl create cluster --name=wanderlust --region=us-east-1 --without-nodegroup
 
-eksctl utils associate-iam-oidc-provider \
-    --region us-east-1 \                        #Change as per your requirement
-    --cluster wanderlust \                      #Change as per your requirement
-    --approve
+eksctl utils associate-iam-oidc-provider --region us-east-1 --cluster wanderlust --approve
 
-eksctl create nodegroup --cluster=wanderlust \  #Change as per your requirement
-                       --region=us-east-1 \     #Change as per your requirement
-                       --name=node2 \
-                       --node-type=t2.medium \  #Change as per your requirement
-                       --nodes=2 \              #Change as per your requirement
-                       --nodes-min=2 \          #Change as per your requirement
-                       --nodes-max=2 \          #Change as per your requirement
-                       --node-volume-size=28 \  #Change as per your requirement
-                       --ssh-access \
-                       --managed \
-                       --asg-access \
-                       --external-dns-access \
-                       --full-ecr-access \
-                       --appmesh-access \
-                       --alb-ingress-access
+eksctl create nodegroup --cluster=wanderlust --region=us-east-1 --node-type=t2.medium --nodes-min=2 --nodes-max=2 --node-volume-size=28
