6768033821a598648ba920a4c7bc5614a065d7fa
root <root@Python.rwnplucb5veuhkqgemu1s0orza.rx.internal.cloudapp.net>
2024-06-14 07:54:49 +0000

    Boto3 Installation

---
+++ Boto3/boto3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install python3 -y
+
+python3 --version
+
+sudo apt install python3-pip -y
+
+sudo apt install python3-boto3 -y
+
+python3 -c "import boto3; print(boto3.__version__)"
+
+
