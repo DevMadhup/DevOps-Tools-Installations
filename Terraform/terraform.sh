2691809557aa4b65559530fe8323e223c87e1730
Madhup Pandey <121779953+DevMadhup@users.noreply.github.com>
2024-08-31 18:07:07 +0530

    Added terraform installation script

---
+++ Terraform/terraform.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Update system package
+sudo apt-get update
+
+#Install GNU software properties and curl packages
+sudo apt-get install -y gnupg software-properties-common -y
+
+#Install the HashiCorp GPG key
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
+#Verify the key's fingerprint
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt update
+sudo apt-get install terraform -y
+
+terraform --version
